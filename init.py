e324dc141ef0d2a31bcb3d57f418bd8e93aa1f65
Jose Silva <siqueirajosesantos19@gmail.com>
2025-07-03 15:51:18 -0300

    <update>: adjust in the entire script and remove step to aws credentials configure

--- init.py
+++ init.py
@@ -1,27 +1,15 @@
 import os
 import sys
 import subprocess
-import boto3
 import time
+import datetime
+
+start_time = datetime.datetime.now()
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
 from utils import terraform as tf_script
 from utils import docker as docker_script
 from utils import ansible as ansible_script
-
-def set_aws_credentials():
-    print("  ℹ️  Define your AWS Credentials")
-    ACCESS_KEY = input("ACCESS_KEY: ")
-    SECRET_KEY = input("SECRET_KEY: ")
-
-    client = boto3.client(
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-    )
-
-    print("  ✅  Credentials defined!")
-    return "Ok"
-    ...
 
 def terraform_func(TERRAFORM_PATH, propose):
     tf_script.start(TERRAFORM_PATH, propose)
@@ -39,28 +27,39 @@ def set_aws_cluster():
 
 def apply_ansible(BASE_PATH, ANSIBLE_PATH):
     ansible_script.start(BASE_PATH, ANSIBLE_PATH)
-    ...
 
 def docker_build(DOCKER_PATH):
     ...
 
-def configure_environment():
+def delete_environment(TERRAFORM_PATH, propose):
+    print("  ℹ️  Cleaning the environment...")
+    print("  ⚠️  Please, don't stop the script execution!\n\n")
+
+    terraform_func(TERRAFORM_PATH, propose)
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"\n  ✅  Environment takes {execution_time.total_seconds()} secods to delete")
+
+def configure_environment(BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, DOCKER_PATH, propose):   
+    print("  ℹ️  Configuring the environment...")
+    print("  ⚠️  Please, don't stop the script execution!\n\n")
+    time.sleep(2)
+
+    terraform_func(TERRAFORM_PATH, propose)
+    set_aws_cluster()
+    apply_ansible(BASE_PATH, ANSIBLE_PATH)
+    #docker_build(DOCKER_PATH)
+
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"\n  ✅  Environment takes {execution_time.total_seconds()} secods to create")
+
+if __name__ == '__main__':
     BASE_PATH = os.getcwd()
     TERRAFORM_PATH  = f'{BASE_PATH}/Terraform'
     ANSIBLE_PATH    = f'{BASE_PATH}/Ansible/playbooks'
     DOCKER_PATH     = f'{BASE_PATH}/Docker'
 
-   
-    print("  ℹ️  Configuring the environment...")
-    print("  ⚠️  Please, don't stop the script execution!\n\n")
-    time.sleep(4)
-    set_aws_credentials()
-    terraform_func(TERRAFORM_PATH, propose="apply")
-    set_aws_cluster()
-    apply_ansible(BASE_PATH, ANSIBLE_PATH)
-    #docker_build(DOCKER_PATH)
-
-if __name__ == '__main__':
     general_options = {
         'Configure Environment': '1',
         'Delete Environment': '0',
@@ -71,5 +70,12 @@ if __name__ == '__main__':
 
     user_choice = int(input("\nSELECT AN OPTION: "))
     if user_choice == 1:
-        configure_environment()
+        configure_environment(BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, DOCKER_PATH, propose="apply")
+        print(f"  ✅  Environment has been created")
     
+    elif user_choice == 0:
+        delete_environment(TERRAFORM_PATH, propose="delete")
+        print(f"  ✅ Environment has been deleted")
+
+    else:
+        print("  ❌  Invalid Option, try again...")
