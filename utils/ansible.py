3bcbc644e5647b77d86240e1d194947456bfa460
Jose Silva <siqueirajosesantos19@gmail.com>
2025-07-03 15:52:01 -0300

    <update>: added a simple script to run ansible playbooks

--- utils/ansible.py
+++ utils/ansible.py
@@ -3,6 +3,7 @@ import os
 import time
 
 def get_argocd_credentials(BASE_PATH):
+    print("  ℹ️  Getting Argo credentials")
     result = subprocess.run(
     ["ansible-playbook", "get-argocd-credentials.yml"],
     stdout=subprocess.PIPE,
@@ -13,6 +14,8 @@ def get_argocd_credentials(BASE_PATH):
         for line in result.stdout.splitlines():
             if "ARGO" in line.replace(' ', ''):
                 file.write(line.strip().replace('"', '').replace(",", "") +"\n")
+    
+    print("  ✅  Credentials are been saved on the following file: argocd_credentials.txt")
 
 
 def apply_ansible_scripts():
@@ -40,8 +43,7 @@ def apply_ansible_scripts():
 
 def start(BASE_PATH, ANSIBLE_PATH):
     os.chdir(ANSIBLE_PATH)
-    #apply_ansible_scripts()
-    print("  ℹ️  Getting Argo credentials")
+    apply_ansible_scripts()
     get_argocd_credentials(BASE_PATH)
 
 if __name__ == '__main__':
