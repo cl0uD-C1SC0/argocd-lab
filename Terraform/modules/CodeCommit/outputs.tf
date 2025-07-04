73f27a381dcfd6a33a833bb09fa8949a90830b28
Jose Silva <siqueirajosesantos19@gmail.com>
2025-07-04 13:56:25 -0300

    <update>: adjust outputs of codecommit

--- Terraform/modules/CodeCommit/outputs.tf
+++ Terraform/modules/CodeCommit/outputs.tf
@@ -9,3 +9,13 @@ output "repository-arn" {
 output "repository-name" {
   value = aws_codecommit_repository.argocd-poc-repository.repository_name
 }
+
+output "codecommit-cred-user" {
+  value = aws_iam_service_specific_credential.codecommit-user-credentials.service_user_name
+  sensitive = true
+}
+
+output "codecommit-cred-password" {
+  value = aws_iam_service_specific_credential.codecommit-user-credentials.service_password
+  sensitive = true
+}
