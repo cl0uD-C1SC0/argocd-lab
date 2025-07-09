a4c6b407c43c7eccc23f71c81b616e4d5b995932
Jose Silva <craftgamer807@gmail.com>
2025-07-09 16:01:11 -0300

    <update>: adjust init script to improve velocity, agility and organize some functions

--- init.py
+++ init.py
@@ -1,77 +1,75 @@
 import os
 import sys
-import subprocess
 import time
 import datetime
 
+sys.path.append(os.path.join(os.path.dirname(__file__), 'common'))
+from common import terraform as terraform_script
+from common import ansible as ansible_script
+from common import codecommit_push as code_push
+from common import argocd
+from common import util
+
+# DON'T CHANGE THESE VARIABLES
+
 start_time = datetime.datetime.now()
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
-from utils import terraform as tf_script
-from utils import ansible as ansible_script
-from utils import codecommit_push as code_push
-from utils import argocd
+BASE_PATH = os.getcwd()
+TERRAFORM_PATH  = f'{BASE_PATH}/Terraform'
+ANSIBLE_PATH    = f'{BASE_PATH}/Ansible/playbooks'
+APP_PATH        = f'{BASE_PATH}/Docker/'
+ARGO_APPS_PATH  = f'{BASE_PATH}/Kubernetes/argo-apps'
+
+general_options = {
+    'Configure Environment': '1',
+    'Delete Environment': '0',
+}
+
+def calc_execution_time():
+    end_time = datetime.datetime.now()
+    execution_time = (end_time - start_time) / 60
+    print(f"\n ✅ Script takes {execution_time.total_seconds()} minutes to Done")
+
+def init_time(message):
+    timer_list = ["1️⃣", "2️⃣", "3️⃣"]
+    print(" ℹ️  May this script need some manual interactions, pay attention please!")
+    print(" ⚠️  Please, don't stop the script execution!\n")
+    for i in timer_list:
+        print(f" ℹ️  {message} the environment in {i}")
+        time.sleep(1)
 
 def set_aws_cluster():
     print(" ℹ️  Updating the Kubeconfig")
-    subprocess.run(
-        ["aws", "eks", "update-kubeconfig", "--name", "argocd-poc-cluster", "--region", "us-east-1"],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL
-    )
-
+    util.run_command(COMMAND="aws eks update-kubeconfig --name argocd-poc-cluster --region us-east-1", shell=True)
     print(" ✅ Kubeconfig has been Updated!")
 
-def delete_environment(BASE_PATH, TERRAFORM_PATH, propose):
-    print(" ℹ️  Cleaning the environment...")
-    print(" ⚠️  Please, don't stop the script execution!\n\n")
+def start_execution(user_choice, BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, ARGO_APPS_PATH):     
+    if user_choice == 1:
+        init_time(message="Initializing")
+        
+        print("\n 🟢 INITIALIZING THE ENVINRONMENT 🟢\n")
+        print(" ℹ️  Configuring the environment...")
 
-    tf_script.start(TERRAFORM_PATH, propose)
-    end_time = datetime.datetime.now()
-    execution_time = end_time - start_time
-    print(f"\n ✅  Environment takes {execution_time.total_seconds()} secods to delete")
+        terraform_script.init_terraform_configs(BASE_PATH, TERRAFORM_PATH)
+        set_aws_cluster()
+        argocd.init_argocd_configs(BASE_PATH, ARGO_APPS_PATH)
+        ansible_script.init_ansible_configs(BASE_PATH, ANSIBLE_PATH)
+        code_push.init_codecommit_configs(BASE_PATH)
+        ansible_script.apply_argocd_apps()
 
-def configure_environment(BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, APP_PATH, ARGO_APPS_PATH, propose):   
-    print(" ℹ️  Configuring the environment...")
-    print(" ℹ️  May this script need some manual interactions, pay attention please!")
-    print(" ⚠️  Please, don't stop the script execution!\n\n")
-    #time.sleep(2)
-
-    tf_script.start(BASE_PATH, TERRAFORM_PATH, propose)
-    set_aws_cluster()
-    argocd.start(ARGO_APPS_PATH)
-    ansible_script.start(BASE_PATH, ANSIBLE_PATH)
-    code_push.start(BASE_PATH, APP_PATH)
-    ansible_script.apply_argocd_apps()
-    
-
-    end_time = datetime.datetime.now()
-    execution_time = end_time - start_time
-    print(f"\n ✅ Environment takes {execution_time.total_seconds()} secods to create")
+        print(f" ✅ Environment has been created")
+        calc_execution_time()
+        return "Environment created"
+    elif user_choice == 0:
+        init_time(message="Deleting")
+        calc_execution_time()
+        return "Environment Deleted"
+    print(" ❌  Invalid Option, try again...")
+    return "Invalid Option, try again"
 
 if __name__ == '__main__':
-    BASE_PATH = os.getcwd()
-    TERRAFORM_PATH  = f'{BASE_PATH}/Terraform'
-    ANSIBLE_PATH    = f'{BASE_PATH}/Ansible/playbooks'
-    APP_PATH        = f'{BASE_PATH}/Docker/'
-    ARGO_APPS_PATH       = f'{BASE_PATH}/Kubernetes/argo-apps'
-
-    general_options = {
-        'Configure Environment': '1',
-        'Delete Environment': '0',
-    }
-
     for option, value in general_options.items():
         print(f"Opção {value}: {option}" )
 
     user_choice = int(input("\nSELECT AN OPTION: "))
-    if user_choice == 1:
-        configure_environment(BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, APP_PATH, ARGO_APPS_PATH, propose="apply")
-        print(f" ✅ Environment has been created")
-    
-    elif user_choice == 0:
-        delete_environment(BASE_PATH, TERRAFORM_PATH, propose="delete")
-        print(f" ✅ Environment has been deleted")
-
-    else:
-        print(" ❌  Invalid Option, try again...")
+    start_execution(user_choice, BASE_PATH, TERRAFORM_PATH, ANSIBLE_PATH, APP_PATH, ARGO_APPS_PATH)
