0229748eb6a959390668c050a2b8d9a962e938de
Jose Silva <craftgamer807@gmail.com>
2025-07-15 17:41:52 -0300

    <update>: adjust script execution and fix some bugs

--- common/undo_environment.py
+++ common/undo_environment.py
@@ -1,8 +1,8 @@
 from util import run_command, change_directory, remove_directory, remove_file
-import os
 
-def delete_tf_files(BASE_PATH):
-    change_directory(f"{BASE_PATH}/Terraform")
+
+def delete_tf_files():
+    
     print(" ℹ️  Removing Terraform Files...")
     files_to_delete = [".terraform.lock.hcl", "terraform.tfstate", "terraform.tfstate.backup"]
     
@@ -15,17 +15,20 @@ def delete_output_files(BASE_PATH):
     change_directory(BASE_PATH)
     files_to_delete = ["argocd_credentials.txt", "terraform_outputs"]
     for file in files_to_delete:
-        remove_file(f"./{file}")
+        remove_file(FILE=f"./{file}")
 
 def destroy_terraform_env():
     print(" ℹ️  Deleting Terraform Environment...")
-    run_command("terraform destroy --auto-approve", shell=True) 
+    result = run_command("terraform destroy --auto-approve", shell=True)
+    print(result)
     print(" ✅ Terraform Environment has been deleted!")
 
-def init_delete_environment(BASE_PATH):
+def init_delete_environment(BASE_PATH, TERRAFORM_PATH):
+    change_directory(TERRAFORM_PATH)
     destroy_terraform_env()
+    delete_tf_files()
     delete_output_files(BASE_PATH)
-    delete_tf_files(BASE_PATH)
+    
 
 if __name__ == '__main__':
     print("Please, run init.py script located in the Root Directory of this repository")
