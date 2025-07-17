9200801a24492a40593a23b63318dd5a82dbc82a
Jose Silva <craftgamer807@gmail.com>
2025-07-17 17:10:19 -0300

    <update>: putting a new default codecommit user

--- Terraform/modules/CodeCommit/variables.tf
+++ Terraform/modules/CodeCommit/variables.tf
@@ -1,7 +1,5 @@
-
-
 variable "codecommit-credentials-user" {
   description = "Variable that's define the name of the user that want's git credentials"
   type = string
-  default = "cloud_user"
+  default = "argocd_user"
 }
