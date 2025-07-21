efbd57338a7090d6e54d3fe042cd0e6e7823ec9d
Jose Silva <craftgamer807@gmail.com>
2025-07-21 13:53:57 -0300

    <update>: change ecr repo from MUTABLE to IMMUTABLE

--- Terraform/modules/ECR/main.tf
+++ Terraform/modules/ECR/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "argocd-ecr-repo" {
   name                 = var.ecr_argocd_repo-name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
